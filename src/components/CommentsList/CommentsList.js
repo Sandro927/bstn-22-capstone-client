8f73dbac66d67223da8cfe86dba2807499967c60
Sandro927 <47231162+Sandro927@users.noreply.github.com>
2022-04-01 23:29:12 -0400

    comments now load from db when button is clicked

--- src/components/CommentsList/CommentsList.js
+++ src/components/CommentsList/CommentsList.js
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import React, { Component } from 'react'
 import AddComment from '../AddComment/AddComment';
 import Comment from '../Comment/Comment';
 import './CommentsList.scss';
 
 export class CommentsList extends Component {
+
+  componentDidMount() {
+    axios.get(`http://localhost:3030/posts/${this.props.postId}/comments`)
+      .then(res => {
+        console.log(res.data)
+      })
+      .catch(err => {
+
+      })
+  }
+
+
   render() {
     return (
       <div className="comments">
